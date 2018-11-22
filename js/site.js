b220c6a170a37c0f45124b3352f24b302ed9a35e
Goulven Champenois <goulven.champenois@ochelys.com>
2018-11-22 09:10:57 +0100

    Combine JS files for (slightly) better performance

--- js/site.js
+++ js/site.js
@@ -1,3 +1,23 @@
+// page-scroll
+$(function() {
+  $('a.page-scroll').bind('click', function(event) {
+    var target = this.hash;
+    var $target = $(target).offset();
+    if ($target) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: $target.top
+      }, 1000, 'easeInOutExpo', function() {
+        window.location.hash = target;
+      });
+    }
+    else {
+      window.location = '/' + target;
+    }
+  });
+});
+
+// animated-header
 var AnimatedHeader = (function () {
 
   var docElem = document.documentElement;
